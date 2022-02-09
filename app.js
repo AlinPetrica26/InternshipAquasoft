cd6e41ccbf6a7eb1151731dc66ff238007ae2170
github_AlinPetrica26 <petricadumitrualin@yahoo.com>
2022-02-09 13:49:20 +0200

    change

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ let x=4;
 let y=11;
 let z= x+y;
 console.log(z);
+
+
+var a1=['Ana' , 'Alex'];
+var a2=['Mihai' , ...a1];
+console.log(a2);
+ 
+function addNumber(x,y){
+    console.log(x+y);
+}
+var nr=[2,4];
+addNumber(...nr);
+
+var arr1 = [1,2,3];
+var arr2 = [4,5,6];
+arr1=[...arr1,...arr2];
+console.log(arr1);
